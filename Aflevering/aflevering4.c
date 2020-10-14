aba7832934de1f8c97960bfbb4a5c93c88cf2c35
Mikkel <milars20@student.aau.dk>
2020-10-14 12:59:09 +0200

    Completed assignment "aflevering4"

--- Aflevering/aflevering4.c
+++ Aflevering/aflevering4.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void)
+void solveQuadraticEquation(double a, double b, double c);
+double computeDiscriminant(double a, double b, double c);
+double findRoot1(double a, double b, double discriminant);
+double findRoot2(double a, double b, double discriminant);
+
+int main(void) 
 {
+ double a, b, c;
 
+ do
+ {
+  printf("Enter coeficients a, b, and c: ");
+  scanf("%lf %lf %lf", &a, &b, &c);
+  if(a == 0 && b == 0 && c == 0)
+  {
+      break;
+  }
+  else
+  {
+      solveQuadraticEquation(a, b, c);  
+  }
+ } while(a != 0 && b != 0 && c != 0);
+ 
+  return 0;
+} 
 
-    return 0;
+void solveQuadraticEquation(double a, double b, double c){
+  double discriminant, root1, root2;
+
+discriminant = computeDiscriminant(a, b, c);
+  if (discriminant < 0)
+    printf("No roots\n");
+  else if (discriminant == 0){
+    root1 = -b/(2*a);
+    printf("One root: %f\n", root1);
+  }
+  else {
+    root1 = (-b + sqrt(discriminant))/(2*a);
+    root2 = (-b - sqrt(discriminant))/(2*a);
+    printf("Two roots: %f and %f\n", root1, root2);
+  }
+}   
+
+double computeDiscriminant(double a, double b, double c)
+{
+    double result;
+
+    result = b * b - 4 * a * c;
+
+    return result;
+}
+
+double findRoot1(double a, double b, double discriminant)
+{
+    return (-b + sqrt(discriminant))/(2*a);
+}
+
+double findRoot2(double a, double b, double discriminant)
+{
+    return (-b - sqrt(discriminant))/(2*a);
 }
