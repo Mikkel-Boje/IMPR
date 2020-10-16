a75f2d1f1cf40f1f9418a095ea4e9d0a0c8aaf59
Mikkel <milars20@student.aau.dk>
2020-10-16 13:24:29 +0200

    Finished the assignment

--- Opgaver/opg6_6.c
+++ Opgaver/opg6_6.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
+double convert(double, double *, double *, double *, double *);
+
 int main(void)
 {
-    double *dollars = 0;
+    double euro, dkk, yen, rubler, dollars;
     
-    printf("How many USD shall I convert for you? ");
-    scanf("%lf", &dollars);
-
+    for (double dollars = 1; dollars <= 100; dollars++)
+    {
+        convert(dollars, &euro, &dkk, &rubler, &yen);
+        printf("%lf dollars is equal to: %lf Euro, %lf DKK, %lf Rubler, %lf Yen \n", dollars, euro, dkk, rubler, yen);
+    }
+    
     return 0;
 }
 
-double convert(double *dollars)
+double convert(double dollars, double *pointer_euro, double *pointer_dkk, double *pointer_rubler, double *pointer_yen)
 {
-    double euro, dkk, yen, rubler, *dollars;
     double dollars_per_euro = 0.89;
     double dollars_per_dkk = 6.66;
     double dollars_per_yen = 119.9;
     double dollars_per_rubler = 66.43;
 
-
-    euro = *dollars * dollars_per_euro;
-    dkk = *dollars * dollars_per_dkk;
-    yen = *dollars * dollars_per_yen;
-    rubler = *dollars * dollars_per_rubler;
-
+    *pointer_euro = dollars * dollars_per_euro;
+    *pointer_dkk = dollars * dollars_per_dkk;
+    *pointer_yen = dollars * dollars_per_yen;
+    *pointer_rubler = dollars * dollars_per_rubler;    
 }
